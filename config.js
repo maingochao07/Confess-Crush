1c53e687ef00678669c260048728c2f0e464416c
maingochao07 <106826895+maingochao07@users.noreply.github.com>
2022-06-03 22:51:54 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: 'Chào cậu:333',
     title: 'Dám yêu anhh khongg🥰',
     desc: 'Dám yêu anhh khongg🥰',
     btnYes: 'Thích lắm <33',
@@ -13,8 +13,8 @@ const CONFIG = {
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Kao biết mà 🥰. Yêu mài nhiều nhiều 😘😘',
+    messDesc: 'Xin lũii vì những lời nói khiến m buồn:((.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://github.com/zukahai/Confess-Crush' // https://www.facebook.com/messages/t/100076566350234
 }
